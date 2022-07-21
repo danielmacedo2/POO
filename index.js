d7b4262a1ba0d9a2edb50109f930279977657168
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-21 14:32:47 -0300

    Method appended

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ class Car {
         console.log(`O carro é ${this.modelo} da marca ${this.marca} do ano de ${this.ano} e a cor dele é ${this.cor}`)
     }
 
+    speed() {
+        console.log(`Car ${this.modelo} getting speed`)
+    }
+
 
 }
 
 const carroX = new Car("Saveiro", "Volkswagen", "2013", "Prata");
 
-carroX.getCarAttributes()
+carroX.getCarAttributes();
+
+carroX.speed();
