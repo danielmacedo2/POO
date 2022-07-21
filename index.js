4d81bb4167846bb856145f42aaf41ec65d026126
Daniel Macedo <danielmsv2011@hotmail.com>
2022-07-21 14:28:53 -0300

    getting car attributes

---
+++ index.js
@@ -0,0 +1,18 @@
+class Car {
+    constructor(modelo, marca, ano, cor) {
+        this.modelo = modelo
+        this.marca = marca
+        this.ano = ano
+        this.cor = cor
+    }
+
+    getCarAttributes() {
+        console.log(`O carro é ${this.modelo} da marca ${this.marca} do ano de ${this.ano} e a cor dele é ${this.cor}`)
+    }
+
+
+}
+
+const carroX = new Car("Saveiro", "Volkswagen", "2013", "Prata");
+
+carroX.getCarAttributes()
